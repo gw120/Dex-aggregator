88f958997eb600c89fd43cc0b16b85df7373d2e0
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-11-27 16:07:27 +0000

    Update Exchanges.js

--- front-end/src/components/Exchanges.js
+++ front-end/src/components/Exchanges.js
@@ -9,25 +9,25 @@ function Exchanges(props) {
 
     const data = useSelector((state) => state.blockchain.value)
     const [amounts, setAmounts] = useState([])
-
     const currentNet = data.network !== "" ? data.network : "Ethereum Mainnet"
 
     async function getPrices() {
 
         if (window.ethereum !== undefined) {
             const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+            const _tokenIn = tokens[currentNet][props.token0]["address"]
+            const _tokenOut = tokens[currentNet][props.token1]["address"]
+            let path = [_tokenIn, _tokenOut]
+
+            const decimals = tokens[currentNet][props.token1]["decimals"]
+
+            let amountIn = utils.parseEther("1", "ether")
+
             const items = await Promise.all(
                 exchanges[currentNet].map(async (e) => {
                     if (e.name !== "Uniswap V3") {
+
                         const router = new ethers.Contract(e.address, e.router.abi, provider)
-
-                        const _tokenIn = tokens[currentNet][props.token0]["address"]
-                        const _tokenOut = tokens[currentNet][props.token1]["address"]
-                        let path = [_tokenIn, _tokenOut]
-
-                        const decimals = tokens[currentNet][props.token1]["decimals"]
-
-                        let amountIn = utils.parseEther("1", "ether")
 
                         try {
                             const amount = await router.getAmountsOut(amountIn, path)
@@ -45,18 +45,10 @@ function Exchanges(props) {
                             return item
                         }
                     } else {
-                        const qouter = new ethers.Contract(e.address, e.router.abi, provider)
-
-                        const _tokenIn = tokens[currentNet][props.token0]["address"]
-                        const _tokenOut = tokens[currentNet][props.token1]["address"]
-                        let path = [_tokenIn, _tokenOut]
-
-                        const decimals = tokens[currentNet][props.token1]["decimals"]
-
-                        let amountIn = utils.parseEther("1", "ether")
+                        const quoter = new ethers.Contract(e.address, e.quoter.abi, provider)
 
                         try {
-                            const amount = await qouter.callStatic.quoteExactInputSingle(
+                            const amount = await quoter.callStatic.quoteExactInputSingle(
                                 _tokenIn,
                                 _tokenOut,
                                 3000,
@@ -99,7 +91,7 @@ function Exchanges(props) {
     return (
 
         <>
-            <Table hover>
+            <Table >
                 <thead>
                     <tr>
                         <th>Exchange</th>
